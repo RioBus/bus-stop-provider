b18da6e0a79470c020034778121d93869cf92245
Mario Cecchi <macecchi@gmail.com>
2016-04-02 11:32:48 -0300

    Add `npm start` script and update default database settings

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
 	busStopApiURL: "http://dadosabertos.rio.rj.gov.br/apiTransporte/Apresentacao/csv/gtfs/onibus/paradas/gtfs_linha$$-paradas.csv",
 	itineraryApiURL: "http://rest.riob.us/v3/itinerary",
 	dataBaseConfig:{
-		user:"riobus",
-		pass:"riobus",
-		dataBaseName:"riobus",
-		host:"mongo",
+		user:"",
+		pass:"",
+		dataBaseName:"nodejs",
+		host:"localhost",
 		port:"27017"
 	}
 };
